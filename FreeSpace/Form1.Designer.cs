a14d731c85b2d9a0a8a4417291522653eff77f33
П - 10 < - 10@DESKTOP-U0IFM7D>
2022-12-09 21:43:25 +0500

    Добавьте файлы проекта.

---
+++ FreeSpace/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace FreeSpace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 25);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(181, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FreeSpace/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace FreeSpace
+{
+    public partial class Form1 : Form
+    {
+        FsProgressBar fsProgress = new FsProgressBar();
+        public override System.Drawing.Color ForeColor { get; set; }
+        private DriveInfo[] allDrives;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void GetSize()
+        {
+            var size = SystemInformation.PrimaryMonitorMaximizedWindowSize;
+            var newSize = new Size(size.Width = 300, size.Height = 150);
+            this.MinimumSize = this.MaximumSize = this.Size = newSize;
+        }
+        private float GetFreeDiskSpace()
+        {
+            allDrives = DriveInfo.GetDrives();
+            double GB = allDrives[0].AvailableFreeSpace / Math.Pow(1024, 3);
+            float _cb = (float)GB;
+            return _cb;
+        }
+        private float GetTotalDiskSpace()
+        {
+            allDrives = DriveInfo.GetDrives();
+            double GB = allDrives[0].TotalSize / Math.Pow(1024, 3);
+            float _cb = (float)GB;
+            return _cb;
+        }
+        private float GetMaximum()
+        {
+            allDrives = DriveInfo.GetDrives();
+            double GB = allDrives[0].TotalSize / Math.Pow(1024, 3);
+            float _cb = (float)GB;
+            return _cb;
+        }
+        private string GetName()
+        {
+            allDrives = DriveInfo.GetDrives();
+            
+            return allDrives[0].Name;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetSize();
+            ShowProgressBar();
+            label1.Text = GetFreeDiskSpace().ToString() + " ГБ свободно из " + GetTotalDiskSpace().ToString();
+        }
+        public void ShowProgressBar()
+        {
+            fsProgress.ForeColor = Color.Red;
+            label2.Text = "Диск " + GetName();
+            progressBar1.Minimum = 0;
+            float _value = GetTotalDiskSpace() - GetFreeDiskSpace();
+            progressBar1.Maximum = (int)GetMaximum();
+            progressBar1.Value += (int)_value;
+        }
+       
+    }
+}
